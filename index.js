6cac5c57796d705a8eb2b01016fedc577c60d2f6
GhosticDev <ghosticman@gmail.com>
2018-07-31 19:52:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ const client = new Discord.Client();
 client.prefix = config.prefix;
 
 client.on("ready", () => {
-	console.log("--------------------------------------------\nBot on with " + client.users.size + " users and " + client.guilds.size + " servers!\n--------------------------------------------");
-	client.user.setActivity(`${client.guilds.get('473892459001282567').memberCount} users!`, {type: 'Watching'});
-	client.channels.get('473893737978920971').edit({
-		name: `Members: ${member.guilds.get('473892459001282567').memberCount}`
-	}).then().catch(console.error);
+	console.log("Bot on with " + client.users.size + " users and " + client.guilds.size + " servers!");
+	client.user.setActivity(`${client.users.size} users!`, {type: 'Watching'});
 });
 
 client.on("message", async message => {
@@ -33,23 +30,48 @@ client.on("message", async message => {
 });
 
 client.on('guildMemberAdd', async member => {
-  if (member.guild.id === '473892459001282567') {
-    client.channels.get('473893737978920971').edit({
+  if (member.guild.id === '461973057247117343') {
+    client.channels.get('471654780922757121').edit({
       name: `Members: ${member.guild.members.size}`
-  	}).then().catch(console.error);
-	}
-  try {
-		client.guilds.get('473892459001282567').members.get(member.id).addRole('473901991945568267');
-  } catch (e) {
-    utils.error(client, e, message.author.username);
-  } finally {}
+    }).then(console.log).catch(console.error);
+    let members = 0;
+    for (var member of member.guild.members.array()) {
+      if (member.user.bot === false) {
+        members++;
+      }
+    }
+    if (member.user.bot) {
+      client.channels.get('471720683085365248').edit({
+        name: `Bots: ${member.guild.members.size-members}`
+      }).then(console.log).catch(console.error);
+    } else {
+      client.channels.get('471722339969662999').edit({
+        name: `Members: ${members}`
+      }).then(console.log).catch(console.error);
+    }
+  }
 });
 
 client.on('guildMemberRemove', async member => {
-  if (member.guild.id === '473892459001282567') {
-    client.channels.get('473893737978920971').edit({
+  if (member.guild.id === '461973057247117343') {
+    client.channels.get('471654780922757121').edit({
       name: `Members: ${member.guild.members.size}`
-    }).then().catch(console.error);
+    }).then(console.log).catch(console.error);
+    let members = 0;
+    for (var member of member.guild.members.array()) {
+      if (member.user.bot === false) {
+        members++;
+      }
+    }
+    if (member.user.bot) {
+      client.channels.get('471720683085365248').edit({
+        name: `Bots: ${member.guild.members.size-members}`
+      }).then(console.log).catch(console.error);
+    } else {
+      client.channels.get('471722339969662999').edit({
+        name: `Members: ${members}`
+      }).then(console.log).catch(console.error);
+    }
   }
 });
 
