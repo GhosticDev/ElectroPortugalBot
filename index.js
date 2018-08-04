f7b32560610563f8040cf83350c86890e2e2dccc
GhosticDev <ghosticman@gmail.com>
2018-08-04 21:37:16 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,10 @@
 const Discord = require('discord.js');
-const config = require("./config.json");
-const utils = require("./utils.js");
 
 const client = new Discord.Client();
-client.prefix = config.prefix;
 
 client.on("ready", () => {
 	console.log("--------------------------------------------\nBot on with " + client.users.size + " users and " + client.guilds.size + " servers!\n--------------------------------------------");
-	client.user.setActivity(`Electro Portugal`, {type: 'Listening'});
-	client.channels.get('473893737978920971').edit({
-		name: `Members: ${client.guilds.get('473892459001282567').memberCount}`
-	}).then().catch(console.error);
-});
-
-client.on("message", async message => {
-  let msg = message.content.toLowerCase();
-	if (message.author.bot) return undefined;
-  let user = message.author;
-
-	if (message.content.indexOf(config.prefix) !== 0) return;
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
-
-  try {
-    let commands = require(`./commands/${command}.js`);
-    commands.run(client, message, args);
-  } catch (e) {
-    utils.error(client, e, message.author.username);
-  } finally {
-  }
-
-});
-
-client.on('guildMemberAdd', async member => {
-  if (member.guild.id === '473892459001282567') {
-    client.channels.get('473893737978920971').edit({
-      name: `Members: ${member.guild.members.size}`
-  	}).then().catch(console.error);
-	}
-  try {
-		client.guilds.get('473892459001282567').members.get(member.id).addRole('473901991945568267');
-  } catch (e) {
-    utils.error(client, e, message.author.username);
-  } finally {}
-});
-
-client.on('guildMemberRemove', async member => {
-  if (member.guild.id === '473892459001282567') {
-    client.channels.get('473893737978920971').edit({
-      name: `Members: ${member.guild.members.size}`
-    }).then().catch(console.error);
-  }
+	client.channels.get('475399952735010837').send("ola");
 });
 
 client.login(process.env.TOKEN);
